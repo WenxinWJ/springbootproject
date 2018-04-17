cd40869383e0e0dcbbf1975efefe05177374b419
WenXinWJ <805671597@qq.com>
2018-04-17 16:51:40 +0800

    分支2 1.5.12版本的配置

--- springbootproject/src/main/java/com/springbootproject/security/JwtTokenUtil.java
+++ springbootproject/src/main/java/com/springbootproject/security/JwtTokenUtil.java
@@ -1,10 +1,5 @@
 package com.springbootproject.security;
 
-import java.io.Serializable;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.function.Function;
 
 import com.springbootproject.security.domain.JwtUser;
 import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
@@ -18,127 +13,146 @@ import org.springframework.mobile.device.Device;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
-/**工具类
- *
+import java.io.Serializable;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
+
+/**
  * @author WenXin
  */
 @Component
 public class JwtTokenUtil implements Serializable {
 
-    private static final long serialVersionUID = -3301605591108950415L;
+  private static final long serialVersionUID = -3301605591108950415L;
 
-    static final String CLAIM_KEY_USERNAME = "sub";
-    static final String CLAIM_KEY_CREATED = "iat";
+  static final String CLAIM_KEY_USERNAME = "sub";
+  static final String CLAIM_KEY_AUDIENCE = "aud";
+  static final String CLAIM_KEY_CREATED = "iat";
 
-    static final String AUDIENCE_UNKNOWN = "unknown";
-    static final String AUDIENCE_WEB = "web";
-    static final String AUDIENCE_MOBILE = "mobile";
-    static final String AUDIENCE_TABLET = "tablet";
+  static final String AUDIENCE_UNKNOWN = "unknown";
+  static final String AUDIENCE_WEB = "web";
+  static final String AUDIENCE_MOBILE = "mobile";
+  static final String AUDIENCE_TABLET = "tablet";
 
-    @SuppressFBWarnings(value = "SE_BAD_FIELD", justification = "It's okay here")
-    private Clock clock = DefaultClock.INSTANCE;
+  @SuppressFBWarnings(value = "SE_BAD_FIELD", justification = "It's okay here")
+  private Clock clock = DefaultClock.INSTANCE;
 
-    @Value("${jwt.secret}")
-    private String secret;
+  @Value("${jwt.secret}")
+  private String secret;
 
-    @Value("${jwt.expiration}")
-    private Long expiration;
+  @Value("${jwt.expiration}")
+  private Long expiration;
 
-    public String getUsernameFromToken(String token) {  // 令牌
-        return getClaimFromToken(token, Claims::getSubject);
+  public String getUsernameFromToken(String token) {
+    return getClaimFromToken(token, Claims::getSubject);
+  }
+
+  public Date getIssuedAtDateFromToken(String token) {
+    return getClaimFromToken(token, Claims::getIssuedAt);
+  }
+
+  public Date getExpirationDateFromToken(String token) {
+    return getClaimFromToken(token, Claims::getExpiration);
+  }
+
+  public String getAudienceFromToken(String token) {
+    return getClaimFromToken(token, Claims::getAudience);
+  }
+
+  public <T> T getClaimFromToken(String token, Function<Claims, T> claimsResolver) {
+    final Claims claims = getAllClaimsFromToken(token);
+    return claimsResolver.apply(claims);
+  }
+
+  private Claims getAllClaimsFromToken(String token) {
+    return Jwts.parser()
+            .setSigningKey(secret)
+            .parseClaimsJws(token)
+            .getBody();
+  }
+
+  private Boolean isTokenExpired(String token) {
+    final Date expiration = getExpirationDateFromToken(token);
+    return expiration.before(clock.now());
+  }
+
+  private Boolean isCreatedBeforeLastPasswordReset(Date created, Date lastPasswordReset) {
+    return (lastPasswordReset != null && created.before(lastPasswordReset));
+  }
+
+  private String generateAudience(Device device) {
+    String audience = AUDIENCE_UNKNOWN;
+    if (device.isNormal()) {
+      audience = AUDIENCE_WEB;
+    } else if (device.isTablet()) {
+      audience = AUDIENCE_TABLET;
+    } else if (device.isMobile()) {
+      audience = AUDIENCE_MOBILE;
     }
+    return audience;
+  }
 
-    public Date getIssuedAtDateFromToken(String token) {
-        return getClaimFromToken(token, Claims::getIssuedAt);
-    }
+  private Boolean ignoreTokenExpiration(String token) {
+    String audience = getAudienceFromToken(token);
+    return (AUDIENCE_TABLET.equals(audience) || AUDIENCE_MOBILE.equals(audience));
+  }
 
-    public Date getExpirationDateFromToken(String token) {
-        return getClaimFromToken(token, Claims::getExpiration);
-    }
+  public String generateToken(UserDetails userDetails, Device device) {
+    Map<String, Object> claims = new HashMap<>();
+    return doGenerateToken(claims, userDetails.getUsername(), generateAudience(device));
+  }
 
-    public String getAudienceFromToken(String token) {
-        return getClaimFromToken(token, Claims::getAudience);
-    }
+  private String doGenerateToken(Map<String, Object> claims, String subject, String audience) {
+    final Date createdDate = clock.now();
+    final Date expirationDate = calculateExpirationDate(createdDate);
 
-    public <T> T getClaimFromToken(String token, Function<Claims, T> claimsResolver) {
-        final Claims claims = getAllClaimsFromToken(token);
-        return claimsResolver.apply(claims);
-    }
+    System.out.println("doGenerateToken " + createdDate);
 
-    private Claims getAllClaimsFromToken(String token) {
-        return Jwts.parser()
-                .setSigningKey(secret)
-                .parseClaimsJws(token)
-                .getBody();
-    }
+    return Jwts.builder()
+            .setClaims(claims) // 声明
+            .setSubject(subject) // 主题
+            .setAudience(audience) // 受众
+            .setIssuedAt(createdDate) // 签发时间
+            .setExpiration(expirationDate) // 过期时间
+            .signWith(SignatureAlgorithm.HS512, secret) // 加密算法、密钥
+            .compact();
+  }
 
-    private Boolean isTokenExpired(String token) {
-        final Date expiration = getExpirationDateFromToken(token);
-        return expiration.before(clock.now());
-    }
+  public Boolean canTokenBeRefreshed(String token, Date lastPasswordReset) {
+    final Date created = getIssuedAtDateFromToken(token);
+    return !isCreatedBeforeLastPasswordReset(created, lastPasswordReset)
+            && (!isTokenExpired(token) || ignoreTokenExpiration(token));
+  }
 
-    private Boolean isCreatedBeforeLastPasswordReset(Date created, Date lastPasswordReset) {
-        return (lastPasswordReset != null && created.before(lastPasswordReset));
-    }
+  public String refreshToken(String token) {
+    final Date createdDate = clock.now();
+    final Date expirationDate = calculateExpirationDate(createdDate);
 
-    private Boolean ignoreTokenExpiration(String token) {
-        // here you specify tokens, for that the expiration is ignored
-        return false;
-    }
+    final Claims claims = getAllClaimsFromToken(token);
+    claims.setIssuedAt(createdDate);
+    claims.setExpiration(expirationDate);
 
-    public String generateToken(UserDetails userDetails) {
-        Map<String, Object> claims = new HashMap<>();
-        return doGenerateToken(claims, userDetails.getUsername());
-    }
+    return Jwts.builder()
+            .setClaims(claims)
+            .signWith(SignatureAlgorithm.HS512, secret)
+            .compact();
+  }
 
-    private String doGenerateToken(Map<String, Object> claims, String subject, String audience) {
-        final Date createdDate = clock.now();
-        final Date expirationDate = calculateExpirationDate(createdDate);
+  public Boolean validateToken(String token, UserDetails userDetails) {
+    JwtUser user = (JwtUser) userDetails;
+    final String username = getUsernameFromToken(token);
+    final Date created = getIssuedAtDateFromToken(token);
+    //final Date expiration = getExpirationDateFromToken(token);
+    return (
+            username.equals(user.getUsername())
+                    && !isTokenExpired(token)
+                    && !isCreatedBeforeLastPasswordReset(created, user.getLastPasswordResetDate())
+    );
+  }
 
-        System.out.println("doGenerateToken " + createdDate);
-
-        return Jwts.builder()
-                .setClaims(claims)  // 声明
-                .setSubject(subject)    // 主题
-                .setIssuedAt(createdDate)   // 签发时间
-                .setExpiration(expirationDate)  // 过期时间
-                .signWith(SignatureAlgorithm.HS512, secret) // 加密算法、密钥
-                .compact();
-    }
-
-    public Boolean canTokenBeRefreshed(String token, Date lastPasswordReset) {
-        final Date created = getIssuedAtDateFromToken(token);
-        return !isCreatedBeforeLastPasswordReset(created, lastPasswordReset)
-                && (!isTokenExpired(token) || ignoreTokenExpiration(token));
-    }
-
-    public String refreshToken(String token) {
-        final Date createdDate = clock.now();
-        final Date expirationDate = calculateExpirationDate(createdDate);
-
-        final Claims claims = getAllClaimsFromToken(token);
-        claims.setIssuedAt(createdDate);
-        claims.setExpiration(expirationDate);
-
-        return Jwts.builder()
-                .setClaims(claims)
-                .signWith(SignatureAlgorithm.HS512, secret)
-                .compact();
-    }
-
-    public Boolean validateToken(String token, UserDetails userDetails) {
-        JwtUser user = (JwtUser) userDetails;
-        final String username = getUsernameFromToken(token);
-        final Date created = getIssuedAtDateFromToken(token);
-        //final Date expiration = getExpirationDateFromToken(token);
-        return (
-                username.equals(user.getUsername())
-                        && !isTokenExpired(token)
-                        && !isCreatedBeforeLastPasswordReset(created, user.getLastPasswordResetDate())
-        );
-    }
-
-    private Date calculateExpirationDate(Date createdDate) {
-        return new Date(createdDate.getTime() + expiration * 1000);
-    }
+  private Date calculateExpirationDate(Date createdDate) {
+    return new Date(createdDate.getTime() + expiration * 1000);
+  }
 }
